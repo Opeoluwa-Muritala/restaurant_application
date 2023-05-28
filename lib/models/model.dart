6245df75f5561d348bb785faade1ba7c76458f39
Tosin2289 <ayoolatosin00@gmail.com>
2023-05-28 16:33:07 +0100

    seventh commit

---
+++ lib/models/model.dart
@@ -0,0 +1,11 @@
+class item {
+  final String name;
+  final String prize;
+  final img;
+  final String description;
+  item(
+      {required this.name,
+      required this.prize,
+      required this.img,
+      required this.description});
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 import 'package:restaurant_application/constants/foods.dart';
 
-class Home extends StatelessWidget {
+import '../constants/tabcontent.dart';
+import '../models/model.dart';
+
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final placeholder = const Opacity(
     opacity: 0,
     child: IconButton(
@@ -11,176 +20,174 @@ class Home extends StatelessWidget {
       icon: Icon(Icons.no_cell),
     ),
   );
+  int index = 0;
+  void currentpage(value) {
+    setState(() {
+      index = value;
+    });
+  }
+
+  static List<item> food = [
+    item(
+        name: 'Spagetti',
+        prize: '\$25',
+        img: 'assets/spag.jpg',
+        description: ''),
+    item(
+        name: 'Macaroni',
+        prize: '\$25',
+        img: 'assets/macaroni.jpg',
+        description: ''),
+    item(
+        name: 'Rosted chicken',
+        prize: '\$35',
+        img: 'assets/chicken.jpg',
+        description: ''),
+    item(
+        name: 'Milkshake',
+        prize: '\$10',
+        img: 'assets/milkshake.jpg',
+        description: ''),
+    item(
+        name: 'Grilled chicken',
+        prize: '\$35',
+        img: 'assets/chik.jpg',
+        description: ''),
+    item(
+        name: 'Burgar',
+        prize: '\$15',
+        img: 'assets/bugar.jpg',
+        description: ''),
+    item(
+        name: 'Turky', prize: '\$45', img: 'assets/turky.png', description: ''),
+  ];
+  List display_list = List.from(food);
+  void searchfood(String value) {
+    setState(() {
+      display_list = food
+          .where((element) =>
+              element.name.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(
-        backgroundColor: Colors.yellow,
-      ),
-      backgroundColor: const Color.fromARGB(255, 206, 205, 205),
-      body: ListView(children: [
-        Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/menu.png'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 30,
-                  ),
-                  const Text(
-                    'Resturant Name',
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Image.asset(
+            "assets/menu.png",
+            color: Colors.brown,
+          ),
+          onPressed: () {},
+        ),
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(
+              Icons.fastfood,
+              color: Colors.brown,
+              size: 20,
             ),
-            const SizedBox(
-              height: 20,
+            SizedBox(
+              width: 5,
             ),
-            Container(
-              height: 200,
-              width: MediaQuery.of(context).size.width * .9,
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                image: DecorationImage(
-                  image: AssetImage('assets/chik.jpg'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Foods(prize: "15 usd", img: 'assets/spag.jpg'),
-                  Foods(prize: "15 usd", img: 'assets/macaroni.jpg'),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Foods(prize: "5 usd", img: 'assets/milkshake.jpg'),
-                  Foods(prize: "10 usd", img: 'assets/bugar.jpg'),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Foods(prize: "25 usd", img: 'assets/turky.png'),
-                  Foods(prize: "25 usd", img: 'assets/chicken.jpg'),
-                ],
-              ),
-            ),
+            Text("Taste Me",
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: Colors.brown,
+                    fontSize: 20)),
           ],
         ),
-      ]),
-      bottomNavigationBar: BottomAppBar(
-        notchMargin: 12,
-        elevation: 0.0,
-        color: const Color.fromARGB(255, 189, 187, 187),
-        shape: const CircularNotchedRectangle(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.home_outlined,
-                      size: 40,
-                    ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.logout,
+                color: Colors.brown,
+              ))
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+            child: Container(
+              decoration: BoxDecoration(boxShadow: [
+                BoxShadow(
+                  blurRadius: 2,
+                  color: Colors.grey.shade300,
+                  spreadRadius: 2,
+                  offset: const Offset(
+                    -2,
+                    2,
                   ),
-                  const Text(''),
-                ],
+                ),
+              ]),
+              child: TextField(
+                onChanged: (value) {
+                  searchfood(value);
+                },
+                decoration: InputDecoration(
+                  hintText: 'Search food',
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: const BorderSide(color: Colors.white)),
+                ),
               ),
             ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.notifications_outlined,
-                    size: 40,
-                  ),
+                tabcontent(
+                  title: "Combo meal",
+                  isactive: true,
+                  onTap: () {},
                 ),
-                const Text(''),
+                tabcontent(
+                  title: "Chicken",
+                  onTap: () {},
+                ),
+                tabcontent(
+                  title: "Drinks",
+                  onTap: () {},
+                ),
+                tabcontent(
+                  title: "Snacks & Sides",
+                  onTap: () {},
+                ),
               ],
             ),
-            placeholder,
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.person_outlined,
-                    size: 40,
-                  ),
-                ),
-                const Text(''),
-              ],
-            ),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.favorite_outline_outlined,
-                    size: 40,
-                  ),
-                ),
-                const Text(''),
-              ],
-            ),
-          ],
-        ),
+          ),
+          Row(
+            children: [],
+          )
+        ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(255, 243, 106, 106),
-        onPressed: () {},
-        child: const Icon(
-          Icons.shopping_cart_outlined,
-          color: Colors.black,
-          size: 40,
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: currentpage,
+        elevation: 0,
+        currentIndex: index,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.brown,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+        ],
       ),
     );
   }
